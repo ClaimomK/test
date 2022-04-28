26f9c85ad7f1bb58de9b4ddd9609ecc8dece84f3
ClaimomK <101823412+ClaimomK@users.noreply.github.com>
2022-04-28 14:12:46 +0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,7 @@
+public Module dateTime{
+        javaTimeModule = new JavaTimeModule();Ja ghhdgghddghvaTimeModule ddd
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
+        return javaTimeModule;
+    }
